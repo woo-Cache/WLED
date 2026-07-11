d163b98eb017da70ed01d798fd9cab3a8e842d9c
woo-Cache <l.grzybowski16@gmail.com>
2026-07-11 13:31:36 +0200

    Update ShtUsermod.h

--- usermods/sht/ShtUsermod.h
+++ usermods/sht/ShtUsermod.h
@@ -118,9 +118,9 @@ class ShtUsermod : public Usermod
 
     bool isEnabled() { return enabled; }
     float getTemperature();
-    float getTemperatureC() { return roundf(shtCurrentTempC * 10.0f) / 10.0f; }
+    float getTemperatureC() { return roundf(shtCurrentTempC * 100.0f) / 100.0f; }
     float getTemperatureF() { return (getTemperatureC() * 1.8f) + 32.0f; }
-    float getHumidity() { return roundf(shtCurrentHumidity * 10.0f) / 10.0f; }
+    float getHumidity() { return roundf(shtCurrentHumidity * 100.0f) / 100.0f; }
     const char* getUnitString();
 
     uint16_t getId() { return USERMOD_ID_SHT; }
